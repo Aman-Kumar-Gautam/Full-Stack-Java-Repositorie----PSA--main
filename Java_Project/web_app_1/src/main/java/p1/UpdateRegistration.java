978a5fccef4df829f23a71ff49a2e19a2f56fd9d
Aman-Kumar-Gautam <you@example.comamangautam862@gmail.com>
2023-10-25 17:22:50 +0530

    25.10.2023

---
+++ Java_Project/web_app_1/src/main/java/p1/UpdateRegistration.java
@@ -0,0 +1,48 @@
+package p1;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/update")
+public class UpdateRegistration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public UpdateRegistration() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	 {
+
+		System.out.println("get");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	{
+		
+		String email = request.getParameter("email");
+		String mobile = request.getParameter("mobile");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=  DriverManager.getConnection("jdbc:mysql://localhost:3306/july_batch_12","root","test");
+			Statement stmnt = con.createStatement();
+			stmnt.executeUpdate("UPDATE registration SET mobile= '"+mobile+"' WHERE email = '"+email+"'");
+		
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		
+	}
+
+}
